5dbe4e987c057cac57fb29911bc5135b9706b2c7
DanielMurphy87 <84238673+DanielMurphy87@users.noreply.github.com>
2023-03-08 16:56:22 +0200

    Implement calling different api in exercise002

--- exercise002.js
+++ exercise002.js
@@ -1,12 +1,12 @@
 import fetch from "node-fetch";
 
-const jsonTypicode = "https://jsonplaceholder.typicode.com/todos/1";
+const jsonTypicode = "https://v2.jokeapi.dev/joke/Programming?type=single";
 
 const fetchData = async (apiEndPoint) => {
   try {
-    const response = await fetch(apiEndPoint);
+    const response = await fetch(apiEndPoint, {method: 'GET'});
     const json = await response.json();
-    console.log(json);
+    console.log(json.joke);
   } catch (error) {
     console.log(error);
   }
